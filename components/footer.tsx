21128d10abf71bf8b838a9c370203e3ee8c5ce30
Al Mithani <almithani@gmail.com>
2025-06-29 16:15:58 -0400

    the build was broken

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
-import { getRoute } from '../utils/routes';
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { Logo } from './logo';
 import { getRoute } from '../utils/routes';
 import { useState } from 'react';
 
